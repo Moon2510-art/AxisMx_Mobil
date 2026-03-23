408221f4a84d892379d3cb2203ad294530d40aa3
CristobalSantana <124050867@upq.edu.mx>
2026-03-23 14:51:56 -0600

    Navegacion
    Archivo de navegacion creado y algunas dependencias a falta de drawer y comenzar a enlazar.

---
+++ AxisMxMb/screens/Perfil.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default function Perfil () {
+    return (
+      <View>
+        <Text>Ah espera de diseno</Text>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({})
